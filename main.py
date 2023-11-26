0ffabf4694b94f1fae207e671a73eef6aa730b85
jason <e5t@yahoo.com>
2023-11-25 17:04:57 -0700

    Dual solutions available

--- main.py
+++ main.py
@@ -15,6 +15,17 @@ grid = [[5,3,0,0,7,0,0,0,0],
         [0,0,0,4,1,9,0,0,5],
         [0,0,0,0,8,0,0,7,9]]
 
+"""grid with two solutions:
+grid = [[5,3,0,0,7,0,0,0,0],
+        [6,0,0,1,9,5,0,0,0],
+        [0,9,8,0,0,0,0,6,0],
+        [8,0,0,0,6,0,0,0,3],
+        [4,0,0,8,0,3,0,0,1],
+        [7,0,0,0,2,0,0,0,6],
+        [0,6,0,0,0,0,2,8,0],
+        [0,0,0,4,1,9,0,0,5],
+        [0,0,0,0,8,0,0,0,0]]"""
+
 c: int = 0
 
 def inc():
@@ -54,10 +65,10 @@ def solve():
                         print(np.matrix(grid),'\n')
                         grid[y][x] = 0
                 return
-    print(np.matrix(grid)," solution")
+    print(np.matrix(grid)," solution"'\n\n\n\n\n\n',)
     input("More?")
 
-print(np.matrix(grid))
+# print(np.matrix(grid))
 solve()
 inc()
 print("Done with the puzzle")
